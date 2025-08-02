84b89ccfb9f4c86fa2092b90215e586400700df3
SMaillis <153215256+SMaillis@users.noreply.github.com>
2025-08-01 22:59:31 -0500

    Add files via upload

---
+++ UWMSmashClubWebsite/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+class HomePage(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+    def post(self, request):
+        pass
